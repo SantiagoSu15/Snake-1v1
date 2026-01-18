a9687b910121e7a7065330ce72252afb08e2f94c
SantiagoSu15 <sasupu08@gmail.com>
2026-01-18 15:06:06 -0500

    Cambios botones de la seleccion de modos

--- src/components/pages/menu.tsx
+++ src/components/pages/menu.tsx
@@ -7,19 +7,19 @@ export const Menu = ()=>{
                 <div className='MenuButtoms'>
                 <Link to="/Snake-1v1/singleplayer">
                     <button className='ModeMenuButton'>
-                        <div className="iconButtonMenu" />
+                        <div className="iconButtonMenu" id="SingleButton"/>
                         <span> Single Player</span> 
                     </button>
                 </Link>
                 <Link to="/Snake-1v1/Multi-local">
                     <button className='ModeMenuButton'>
-                        <div className="iconButtonMenu" />
+                        <div className="iconButtonMenu" id="LocalButton" />
                         <span> Multi Local</span> 
                     </button>
                 </Link>
                 <Link to="/Snake-1v1/Multi-online">
                     <button className='ModeMenuButton'>
-                        <div className="iconButtonMenu" />
+                        <div className="iconButtonMenu" id="MultiButton" />
                         <span> Multi Online</span> 
                     </button>
                 </Link>
